261589c83a20e3a4d01563bc2fca089efa4c516f
Azphw <mth29000@gmail.com>
2013-04-19 13:55:46 +0300

    Create Wintel.java

---
+++ Wintel.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+
+
+public class Wintel extends JFrame{
+
+  private JPanel IDHaut;
+	private JPanel IDEst;
+	private JPanel ID;
+	private JLabel Abonnée;
+	private JLabel Prenom;
+        private JMenuBar Menu;
+
+	public Wintel() {
+
+	     super ( "Wintel" );
+	     this.creerInterface();
+	     this.attacherReactions();
+	     this.setSize( 500, 500 );
+	     this.setVisible( true );
+	     this.setDefaultCloseOperation ( EXIT_ON_CLOSE );
+		
+	     
+	}
+
+	public void creerInterface(){
+		this.IDHaut = new JPanel (new GridLayout(1,2));
+		this.Menu = new JMenuBar ();
+		this.Prenom = new JLabel("Prenom");
+		
+		this.add(Prenom);
+		
+
+	}
+
+	private void attacherReactions(){}
+}
